5074aeccf1484258e82aadbabeb1ac8ce410ceba
Thomas Icarangal <47007330+Tomubi@users.noreply.github.com>
2023-11-07 19:45:09 -0500

    feat: create Physician model

---
+++ Domain/Physician.cs
@@ -0,0 +1,11 @@
+namespace Domain
+{
+    public class Physician
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+        public DateTime Date { get; set; }
+        public string City { get; set; }
+    }
+}
