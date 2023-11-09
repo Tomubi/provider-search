08f46fcfc7c333924540499ee7d4fdb28fbffacd
Thomas Icarangal <47007330+Tomubi@users.noreply.github.com>
2023-11-09 17:11:56 -0500

    feat: connect react app to API

--- provider-search-client/src/App.tsx
+++ provider-search-client/src/App.tsx
@@ -3,15 +3,24 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [providers, setProviders] = useState();
+  const [providers, setProviders] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/providers").then((response) => {
+    axios.get("http://localhost:5000/api/physicians").then((response) => {
       setProviders(response.data);
     });
-  });
+  }, []);
 
-  return <h1>Provider Search</h1>;
+  return (
+    <div>
+      <h1>Provider Search</h1>
+      <ul>
+        {providers.map((provider: any) => (
+          <li key={provider.id}>{provider.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
